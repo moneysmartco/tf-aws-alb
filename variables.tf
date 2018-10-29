0befd4661fcb0466ad04d71a74f9abca0626bc55
Tung Nguyen <tungns.inf@gmail.com>
2018-10-29 15:03:34 +0800

    We don't use alb_listener_default_http_arn anymore

--- variables.tf
+++ variables.tf
@@ -7,9 +7,7 @@ variable "alb_ssl_cert_arn"                   {}
 variable "alb_listener_http_count"            {
   default = true
 }
-variable "alb_listener_default_http_arn"      {
-  default = ""
-}
+
 variable "alb_listener_https_count"           {
   default = true
 }
