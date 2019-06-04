fda45d19d1b7a68a062b8e4d3f4d0579a17cc0a2
Brian <tjtbrian@gmail.com>
2019-06-04 17:42:33 +0800

    add arn_suffix output

--- output.tf
+++ output.tf
@@ -2,6 +2,9 @@ output "alb_arn" {
   value = "${aws_alb.alb.arn}"
 }
 
+output "alb_arn_suffix" {
+  value = "${aws_alb.alb.arn_suffix}"
+}
 output "alb_sg_id" {
   value = "${aws_security_group.alb_sg.id}"
 }
