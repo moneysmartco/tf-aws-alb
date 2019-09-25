eb77ce2a5633f0c59ce41a300e4aa9f83051663f
Eugene Ho <47513654+eugeneho-moneysmart@users.noreply.github.com>
2019-09-25 17:46:30 +0800

    parse string instead of list in security group source id

--- main.tf
+++ main.tf
@@ -99,10 +99,9 @@ resource "aws_security_group_rule" "allow_https_source_security_group" {
   from_port                 = 443
   to_port                   = 443
   protocol                  = "tcp"
-  source_security_group_id  = "${split(",", var.alb_ingress_source_security_group_ids)}"
+  source_security_group_id = "${element(var.alb_ingress_source_security_group_ids, count.index)}"
   security_group_id         = "${aws_security_group.alb_sg.id}"
 }
-
 
 #--------------------
 # ALB
