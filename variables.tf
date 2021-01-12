2a7d8bba638476e080557da57c2af9f4cb622dda
alex arockiaraj <alex@francium.tech>
2021-01-12 13:53:16 +0530

    Upgrade to terraform 0.12

--- variables.tf
+++ variables.tf
@@ -1,21 +1,27 @@
-variable "env"                                {}
+variable "env" {
+}
 
-variable "vpc_id"                             {}
+variable "vpc_id" {
+}
 
-variable "public_subnet_ids"                  {}
+variable "public_subnet_ids" {
+}
 
-variable "project_name"                       {}
+variable "project_name" {
+}
 
-variable "alb_name"                           {}
+variable "alb_name" {
+}
 
-variable "alb_ssl_cert_arn"                   {}
+variable "alb_ssl_cert_arn" {
+}
 
-variable "alb_listener_https_count"           {
+variable "alb_listener_https_count" {
   description = "Use alb_listener_default_https_arn as default target group on ALB"
   default     = true
 }
 
-variable "alb_listener_default_https_arn"     {
+variable "alb_listener_default_https_arn" {
   description = "Default target group for HTTPS listener"
   default     = ""
 }
@@ -27,48 +33,49 @@ variable "alb_listener_ssl_policy" {
 
 variable "alb_internal" {
   description = "Set LB to be private or public"
-  default = false
+  default     = false
 }
 
 variable "alb_enable_deletion_protection" {
   description = "If true, deletion of the load balancer will be disabled via the AWS API. This will prevent Terraform from deleting the load balancer"
-  default = true
+  default     = true
 }
 
 variable "alb_s3_access_log_bucket" {
   description = "S3 bucket name to store ALB access logs"
-  default = ""
+  default     = ""
 }
 
 variable "alb_s3_access_log_enabled" {
   description = "Boolean to enable / disable ALB access logs"
-  default = false
+  default     = false
 }
 
 variable "alb_ingress_whitelist_ips" {
   description = "List of IPs that ALB allows to connect to. Use comma to separate multiple IPs"
-  type = "string"
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "alb_ingress_source_security_group_ids" {
   description = "List of source Security Group IDs that ALB allows to connect to. Use comma to separate multiple Ids"
-  type = "list"
-  default = []
+  type        = list(string)
+  default     = []
 }
 
 variable "tags" {
   description = "Tagging resources with default values"
   default = {
-    "Name" = ""
-    "Country" = ""
+    "Name"        = ""
+    "Country"     = ""
     "Environment" = ""
-    "Repository" = ""
-    "Owner" = ""
-    "Department" = ""
-    "Team" = "shared"
-    "Product" = "common"
-    "Project" = "common"
-    "Stack" = ""
+    "Repository"  = ""
+    "Owner"       = ""
+    "Department"  = ""
+    "Team"        = "shared"
+    "Product"     = "common"
+    "Project"     = "common"
+    "Stack"       = ""
   }
 }
+
